a88e5ec77f2506430acfd4f8123c9091bb4c8115
Tanish Pandey <tanishpandey14@gmail.com>
2024-04-10 20:43:19 +0530

    footer of home page

--- src/Components/Content/ContentFoot.jsx
+++ src/Components/Content/ContentFoot.jsx
@@ -2,34 +2,34 @@ import React from 'react'
 
 function ContentFoot() {
   return (
-    <div className='h-[350px] w-full bg-zinc-900 flex items-end justify-center'>
-        <div className='h-[250px] w-[80%]  text-3xl   p-10  text-white'>
-        <i class="ml-14 fa-brands fa-facebook"></i>
-        <i class="ml-4 fa-brands fa-instagram"></i>
-        <i class="ml-4 fa-brands fa-twitter"></i>
-        <i class="ml-4 fa-brands fa-youtube"></i>
-        <div className='flex gap-10 mt-6 text-base '>
+    <div className='h-[300px] w-full bg-black flex items-start justify-center'>
+        <div className='h-[250px] w-[85%]  text-2xl p-10  text-zinc-300'>
+        <i class="text-white ml-14 fa-brands fa-facebook"></i>
+        <i class="text-white ml-8 fa-brands fa-instagram"></i>
+        <i class="text-white ml-8 fa-brands fa-twitter"></i>
+        <i class="text-white ml-8 fa-brands fa-youtube"></i>
+        <div className='flex gap-10 mt-6 text-base font-thin '>
 
-        <ul className=' px-14'>
+        <ul className=' px-14 leading-loose '>
             <li>Audio Description</li>
             <li>Investors Relations</li>
             <li>Legal Notices</li>
-            <li></li>
+            <li className='border-2 mt-6 w-26 h-8 text-center  hover:text-white '>Service box</li>
         </ul>
-        <ul className=' px-14'>
+        <ul className=' px-14 leading-loose'>
             <li>Help Center</li>
             <li>Jobs</li>
             <li>Cookie Preferences</li>
             <li></li>
         </ul>
-        <ul className=' px-14'>
+        <ul className=' px-14 leading-loose'>
             <li>Gift Cards</li>
             <li>Terms of Use</li>
             <li>Corporate Information</li>
             <li></li>
         </ul>
-        <ul className=' px-14'>
-            <li>Madia Center</li>
+        <ul className=' px-14 leading-loose'>
+            <li>Media Center</li>
             <li>Privacy</li>
             <li>Contact Us</li>
             <li></li>
