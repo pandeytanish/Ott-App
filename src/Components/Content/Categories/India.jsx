37ad236bfafc36676c81e34eaee93160db462430
Tanish Pandey <tanishpandey14@gmail.com>
2024-04-11 20:46:34 +0530

    css optimization

--- src/Components/Content/Categories/India.jsx
+++ src/Components/Content/Categories/India.jsx
@@ -11,17 +11,21 @@ function India() {
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % dataLength);
   };
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1) % dataLength);
+  };
 
   return (
     <div >
     <h1 className='text-white font-extrabold text-2xl  ml-14 mt-[40px]'>Made In India </h1>
     <div className='h-[200px] w-[95%] ml-[50px]  flex gap-2 '>
+    <button className='ml-2 mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={prevSlide}> <i class="fa-solid fa-chevron-left"></i></button>
     {IndiaApi.slice(currentIndex, currentIndex + 6).map((value,index) =>
-    <div key={index} className='h-[140px] w-[220px]  bg-slate-200 mt-5 rounded-md hover:border-4  hover:border-red-500 '>
-      <img className='h-[140px] w-[220px] rounded-md ' src={value.img} alt={`Popular ${index}`}/>
+    <div key={index} className='h-[140px] w-[260px]  bg-slate-200 mt-5 rounded-md hover:border-4  hover:border-red-500 '>
+      <img className='h-[140px] w-[260px] rounded-md ' src={value.img} alt={`Popular ${index}`}/>
     </div>
     )}
-    <button className='ml-6 mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={nextSlide}>></button>
+    <button className='ml-6 mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={nextSlide}><i class="fa-solid fa-chevron-right"></i></button>
 </div>
 
 
--- src/Components/Content/Categories/Popular.jsx
+++ src/Components/Content/Categories/Popular.jsx
@@ -8,6 +8,9 @@ function Popular() {
   const nextSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % dataLength);
   };
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1) % dataLength);
+  };
 
   return (
     <>
@@ -16,16 +19,13 @@ function Popular() {
           Popular On Netflix
         </h1>
         <div className='h-[200px] w-[95%] ml-[50px] flex gap-2  '>
+        <button className='ml-2  mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={prevSlide}> <i class="fa-solid fa-chevron-left"></i> </button>
           {popularApi.slice(currentIndex, currentIndex + 6).map((value, index) => (
-            <div key={index} className='h-[140px] w-[220px]  bg-slate-200 mt-5 rounded-md hover:border-4  hover:border-red-500'>
-              <img className='h-[140px] w-[220px] rounded-md ' src={value.img} alt={`Popular ${index}`} />
-              {/* <i className='hidden' class="fa-solid fa-circle-play "></i>
-              <i class="fa-solid fa-circle-plus"></i>
-              <i class="fa-regular fa-thumbs-up"></i>
-              <i class="fa-regular fa-circle-chevron-down"></i> */}
+            <div key={index} className='h-[140px] w-[260px]   bg-slate-200 mt-5 rounded-md hover:border-4  hover:border-red-500'>
+              <img className='h-[140px] w-[260px] rounded-md  ' src={value.img} alt={`Popular ${index}`} />
             </div>
           ))}
-          <button className='ml-6 mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={nextSlide}>></button>
+          <button className='ml-6 mb-6 text-3xl text-white font-extrabold hover:text-white hover:text-4xl' onClick={nextSlide}><i class="fa-solid fa-chevron-right"></i></button>
         </div>
       </div>
     </>
